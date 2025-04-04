48578c68c6790ee4414e431588abce08555af3b1
Isaac Bilsel <isaacbilsel@shell.seas.gwu.edu>
2025-04-04 14:11:02 -0400

    Still needs reset synchronization and log file and synthesis

--- project7/simulation/scripts/design_files_syn.sh
+++ project7/simulation/scripts/design_files_syn.sh
@@ -2,5 +2,5 @@
 
 # list of design files to be compiled
 
-ver_files=("../../synthesis/netlists/fir_filter_syn.v"
-	   "../testbench/fir_filter_syn_tb.v")	
+ver_files=("../../synthesis/netlists/TopModule_syn.v"
+	   "../testbench/TopModule_tb.v")	
