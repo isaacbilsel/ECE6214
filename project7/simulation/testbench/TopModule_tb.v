312deebd431089c9ba6256d27d2b892aba9e0586
Isaac Bilsel <isaacbilsel@shell.seas.gwu.edu>
2025-04-04 13:50:46 -0400

    TopModule testbench simulation looks correct. Still needs log file to be written in test bench. TopModule also needs reset synchronization

--- project7/simulation/testbench/TopModule_tb.v
+++ project7/simulation/testbench/TopModule_tb.v
@@ -7,20 +7,22 @@ module TopModule_tb;
     // reg upsample_clk;
     reg rst_n;
     reg [3:0] data_in;
-    reg data_valid;
     reg coeff_write;
     reg [6:0] coeff_addr;
     reg [7:0] coeff_in;
+	reg [3:0] sample_rate;
+	reg new_symbol;
     wire [11:0] data_out;
     wire data_out_valid;
+
      
     // Instantiate the DUT (Device Under Test)
     TopModule dut (
         .clk(clk),
-	// .upsample_clk(upsample_clk),
         .rst_n(rst_n),
+        .sample_rate(sample_rate),
         .data_in(data_in),
-        .data_valid(data_valid),
+        .new_symbol(new_symbol),
         .coeff_write(coeff_write),
         .coeff_addr(coeff_addr),
         .coeff_in(coeff_in),
@@ -28,52 +30,63 @@ module TopModule_tb;
         .data_out_valid(data_out_valid)
     );
     
-    // Clock Generation
     // Clock frequency: 40MhZ - 130MHz
     // Clock Period: 25 ns -7.7 ns
-    // always #5 upsample_clk = ~upsample_clk; // 10ns clock period
-    always #5 clk = ~clk; 	// 100 ns clock
+    // always #5 upsample_clk = ~upsample_clk; 
+    always #5 clk = ~clk; 	// 10 ns clock
 
+	// testbench signals
+	reg [8*39:0]       testcase;
+	integer 	      i;
+	   
     // Testbench Procedure
     initial begin
-        // Initialize Signals
-        clk = 0;
-        upsample_clk = 0;
-	rst_n = 0;
-        data_in = 0;
-        data_valid = 0;
-        coeff_write_enable = 0;
-        coeff_addr = 0;
-        coeff_data = 0;
-        
-        // Reset Pulse
-        #200 rst_n = 1;
-        
-        // Load Coefficients
-        #100 coeff_write_enable = 1;
-        coeff_addr = 7'd0;
-        coeff_data = 8'd5;
-        #10 coeff_write_enable = 0;
-        
-        #100 coeff_write_enable = 1;
-        coeff_addr = 7'd1;
-        coeff_data = 8'd10;
-        #100 coeff_write_enable = 0;
-        
-        // Apply Input Data
-        #200 data_in = 4'b0011;
-        data_valid = 1;
-        #100 data_valid = 0;
-        
-        #200 data_in = 4'b0101;
-        data_valid = 1;
-        #100 data_valid = 0;
-        
-        // Wait for Output Processing
-        #10000;
-        
-        // Finish Simulation
-        $stop;
+		testcase = "Initializing";
+		clk <= 0;
+		rst_n <= 0;
+		sample_rate <= 4'd13;
+		repeat(2) @(posedge clk);
+		rst_n = 1;
+		repeat(2) @(posedge clk);
+		
+		// generate data to write into coeff memory
+		for (i=0; i <=70; i = i+1) begin
+			coeff_write = 1'b1;
+			coeff_addr  = i;
+			coeff_in    = i;
+			@(negedge clk);	 
+		end
+
+		coeff_write = 1'b0;
+
+		// flush the pipeline
+		repeat(10) @(posedge clk);
+
+		// simulate impulse response  
+		testcase = "Impulse";
+		@(negedge clk);
+		data_in = 4'h1;
+		new_symbol = 1'b1;
+		repeat (2)@(negedge clk);
+		data_in = 4'h0;
+		new_symbol = 1'b0;
+		@(negedge clk);
+		repeat(150)
+		@(posedge clk);
+		@(negedge clk);	
+		
+		// simulate step response
+		testcase = "Step";
+		new_symbol = 1'b1;
+		data_in = 4'h1;
+		repeat(150)
+		@(posedge clk);
+
+		// flush the pipeline
+		data_in = 4'h0;
+		repeat(150)
+		@(posedge clk);
+		$finish;
     end
     
 endmodule
--- project7/simulation/work/xcelium.d/run.lnx8664.17.10.d/files.ts
+++ project7/simulation/work/xcelium.d/run.lnx8664.17.10.d/files.ts
@@ -2,8 +2,8 @@
 1713473524 /home/ead/isaacbilsel/ece6214/ECE6214/project7/source/dsp_slice.v
 1713473524 /home/ead/isaacbilsel/ece6214/ECE6214/project7/source/coeff_memory.v
 1743722750 /home/ead/isaacbilsel/ece6214/ECE6214/project7/source/fir_filter.v
-1743785893 /home/ead/isaacbilsel/ece6214/ECE6214/project7/source/TopModule.v
-1743646229 /home/ead/isaacbilsel/ece6214/ECE6214/project7/simulation/testbench/TopModule_tb.v
+1743788451 /home/ead/isaacbilsel/ece6214/ECE6214/project7/source/TopModule.v
+1743788730 /home/ead/isaacbilsel/ece6214/ECE6214/project7/simulation/testbench/TopModule_tb.v
 1743780981 /home/ead/isaacbilsel/ece6214/ECE6214/project7/simulation/testbench/fir_filter_tb.v
 1743727273 /home/ead/isaacbilsel/ece6214/ECE6214/project7/source/upsampler.v
-1743727231 /home/ead/isaacbilsel/ece6214/ECE6214/project7/simulation/testbench/upsampler_tb.v
+1743786948 /home/ead/isaacbilsel/ece6214/ECE6214/project7/simulation/testbench/upsampler_tb.v
