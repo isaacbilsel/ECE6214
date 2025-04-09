3b4256c433e2348a870e8f3c98cef5f296bed67a
isaacbilsel <isaac.bilsel@gmail.com>
2025-04-09 15:54:42 -0400

    "naming"

--- project8/source/modulator_64qam_top.v
+++ project8/source/modulator_64qam_top.v
@@ -1,3 +1,45 @@
+`timescale 1ns / 1ps
+module modulator_64qam_top(
+			   input wire SCLK,
+			   input wire MOSI,
+			   input wire CSN,
+			   input wire rst_n,
+			   output wire MISO,
+			   output wire MISO_enable
+			   );
+
+   //internal signals
+   wire [15:0]			       reg_read_data;
+   wire [7:0]			       reg_address;
+   wire [15:0]			       reg_write_data;
+   wire				       reg_write_enable;
+   wire				       reg_read_enable;
+
+   //interfacing
+   SPI_Interface cmd_interface(
+			       .SCLK(SCLK),
+			       .MOSI(MOSI),
+			       .CSN(CSN),
+			       .rst_n(rst_n),
+			       .reg_read_data(reg_read_data),
+			       .MISO(MISO),
+			       .MISO_enable(MISO_enable),
+			       .reg_address(reg_address),
+			       .reg_write_data(reg_write_data),
+			       .reg_write_enable(reg_write_enable),
+			       .reg_read_enable(reg_read_enable)
+			       );
+
+   register_array memory_array(
+			       .SCLK(SCLK),
+			       .rst_n(rst_n),
+			       .reg_address(reg_address),
+			       .reg_write_data(reg_write_data),
+			       .reg_write_enable(reg_write_enable),
+			       .reg_read_enable(reg_read_enable),
+			       .reg_read_data(reg_read_data)
+			       );
+endmodule // 64QAM_Modulator_top
 
 module SPI_Interface(
 		     input wire	       SCLK,
