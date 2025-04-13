aa1c7d9dab72c7581c11ec75d5e29fc9835598f4
isaacbilsel <isaac.bilsel@gmail.com>
2025-04-12 23:28:11 -0400

    topmodule skeleton updated

--- project8/source/qam_top.v
+++ project8/source/qam_top.v
@@ -1,42 +1,58 @@
 `timescale 1ns / 1ps
 module qam_top(
-			   input wire SCLK,
-			   input wire MOSI,
-			   input wire CSN,
-			   input wire rst_n,
-			   output wire MISO,
-			   output wire MISO_enable
-			   );
+	input wire SCLK,
+	input wire dsp_clk,
+	input wire MOSI,
+	input wire CSN,
+	input wire rst_n,
+	input wire [3:0] I,
+	input wire [3:0] Q,
 
-   //internal signals
-   wire [7:0]			       reg_read_data;
-   wire [9:0]			       reg_address;
-   wire [7:0]			       reg_write_data;
-   wire				       reg_write_enable;
-   wire				       reg_read_enable;
+	output wire MISO,
+	output wire MISO_enable,
+	output wire [9:0] I_out,
+	output wire [9:0] Q_out
+	);
 
-   //interfacing
-   SPI_Interface cmd_interface(
-			       .SCLK(SCLK),
-			       .MOSI(MOSI),
-			       .CSN(CSN),
-			       .rst_n(rst_n),
-			       .reg_read_data(reg_read_data),
-			       .MISO(MISO),
-			       .MISO_enable(MISO_enable),
-			       .reg_address(reg_address),
-			       .reg_write_data(reg_write_data),
-			       .reg_write_enable(reg_write_enable),
-			       .reg_read_enable(reg_read_enable)
-			       );
+	//internal signals
+	wire [7:0]			       reg_read_data;
+	wire [9:0]			       reg_address;
+	wire [7:0]			       reg_write_data;
+	wire				       reg_write_enable;
+	wire				       reg_read_enable;
 
-   register_array memory_array(
-			       .SCLK(SCLK),
-			       .rst_n(rst_n),
-			       .reg_address(reg_address),
-			       .reg_write_data(reg_write_data),
-			       .reg_write_enable(reg_write_enable),
-			       .reg_read_enable(reg_read_enable),
-			       .reg_read_data(reg_read_data)
-			       );
+	//interfacing
+	SPI_Interface cmd_interface(
+		.SCLK(SCLK),
+		.MOSI(MOSI),
+		.CSN(CSN),
+		.rst_n(rst_n),
+		.reg_read_data(reg_read_data),
+		.MISO(MISO),
+		.MISO_enable(MISO_enable),
+		.reg_address(reg_address),
+		.reg_write_data(reg_write_data),
+		.reg_write_enable(reg_write_enable),
+		.reg_read_enable(reg_read_enable)
+	);
+
+	// CDC module here
+	
+
+	dsp_top dsp (
+		.clk(dsp_clk),
+        .rst_n(rst_n),
+		.msg_in(~CSN),		// This might be wrong
+        .sample_rate(sample_rate),
+        .data_in_i(I),
+		.data_in_q(Q),
+        .new_symbol(I || Q), // This may also need to be set in tb
+        .coeff_rw(coeff_rw),		// CDC output
+        .coeff_addr(coeff_addr),	// CDC output/output
+        .coeff_in(coeff_in),		// CDC output
+        .I_out(I_out),
+		.Q_out(Q_out),
+		.coeff_read_out(coeff_read_out) // CDC input
+	)
+
 endmodule // qam_top
