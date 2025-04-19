b346ceaa6bdb6a85f3359e74dabbb5c96b4be114
isaacbilsel <isaac.bilsel@gmail.com>
2025-04-19 17:05:15 -0400

    Added sample storage module, needs to be tested

--- project8/source/dsp_top.v
+++ project8/source/dsp_top.v
@@ -5,14 +5,14 @@ module dsp_top(
     input wire [3:0] sample_rate,
     input [3:0] data_in_i,     	// 4-bit input data from the symbol generator.
     input [3:0] data_in_q,
-    input coeff_rw,
+    input rw,
     input msg_in,
     input [7:0] coeff_in, 		// We may need to make this a two dimensional array
-    input [9:0] coeff_addr,
+    input [9:0] mem_addr,
     input wire new_symbol, 		
     output wire [9:0] I_out,  // Inphase 10 bit output to DAC
     output wire [9:0] Q_out,  // Quadrature 10 bit output to DAC
-    output reg [7:0] coeff_read_out
+    output reg [7:0] mem_read_out
 );
 
     // Internal signals
@@ -23,20 +23,27 @@ module dsp_top(
     wire rst_n_sync_wire;
 
     // SPI communication internal signals
-    wire coeff_write;
-    wire coeff_read;
+    wire write;
+    wire read;
     wire [9:0] coeff_i_addr;
     wire [9:0] coeff_q_addr;
     wire [7:0] coeff_read_out_i;
     wire [7:0] coeff_read_out_q;
-    reg [7:0] coeff_read_out_next;
+    wire [7:0] sample_read_out_i;
+    wire [7:0] sample_read_out_q;
+
+    reg [7:0] mem_read_out_next;
 
     // Map global address to a local address in coefficient memory of Q or I filter
-    assign coeff_i_addr = (coeff_addr > 10'd127 && coeff_addr < 10'd199) ? (coeff_addr-128) : 10'd0;
-    assign coeff_q_addr = (coeff_addr > 10'd255 && coeff_addr < 10'd327) ? (coeff_addr-256) : 10'd0;
+    assign coeff_i_addr = (mem_addr > 10'd127 && mem_addr < 10'd199) ? (mem_addr-128) : 10'd0;
+    assign coeff_q_addr = (mem_addr > 10'd255 && mem_addr < 10'd327) ? (mem_addr-256) : 10'd0;
 
-    assign coeff_read = msg_in ? ~coeff_rw : 0;
-    assign coeff_write = msg_in ? coeff_rw: 0;
+    // Map global address to local address of output memory of Q or I filter
+    assign sample_i_addr = (mem_addr > 10'd511 && mem_addr < 10'd640) ? (mem_addr-512) : 10'd0;
+    assign sample_q_addr = (mem_addr > 10'd767 && mem_addr < 10'd896) ? (mem_addr-768) : 10'd0;
+
+    assign read = msg_in ? ~rw : 0;
+    assign write = msg_in ? rw: 0;
 
     // Time taken for filter to propagate output through and flush completely
 	// Each filter has 4 CC latency, filters are pipelined, so
@@ -74,11 +81,11 @@ module dsp_top(
 		.output_data_2(upsampler_out_q)
 	);
 
-    // FIR Filter instantiation for i
+    // FIR Filter instantiation
     fir_filter i_fir_filter(
         .clk(clk),
 		.sample_in(upsampler_out_i),
-        .coeff_read(coeff_read),
+        .coeff_read(read),
         .coeff_write(coeff_write),
         .coeff_in(coeff_in),
         .coeff_addr(coeff_i_addr),
@@ -89,12 +96,33 @@ module dsp_top(
     fir_filter q_fir_filter(
         .clk(clk),
 		.sample_in(upsampler_out_q),
-        .coeff_read(coeff_read),
+        .coeff_read(read),
         .coeff_write(coeff_write),
         .coeff_in(coeff_in),
         .coeff_addr(coeff_q_addr),
         .fir_out(filter_out_q),
         .coeff_read_out(coeff_read_out_q)
+    );
+
+    // Sample Storage Instatiation
+    sample_storate i_sample_storage (
+        .clk(clk),
+        .rst_n(rst_n_sync_wire),
+        .sample_in(filter_out_i),
+        .sample_addr(sample_i_addr),
+        .sample_read(read),    
+        .counter_begin(),               // Fix this 
+        .sample_read_out(sample_read_out_i)
+    );
+
+    sample_storage q_sample_storage (
+        .clk(clk),
+        .rst_n(rst_n_sync_wire),
+        .sample_in(filter_out_q),
+        .sample_addr(sample_q_addr),
+        .sample_read(read),   
+        .counter_begin(),               // Fix this
+        .sample_read_out(sample_read_out_q)
     );
 
 	// Logic for Datapath reset
@@ -107,7 +135,7 @@ module dsp_top(
         else begin
             counter <= counter_next;
             data_out_valid <= data_out_valid_next;
-            coeff_read_out <= coeff_read_out_next;
+            mem_read_out <= mem_read_out_next;
             state <= state_next;
         end
     end
@@ -115,7 +143,7 @@ module dsp_top(
 	always @(*) begin
 		data_out_valid_next <= data_out_valid;
 		counter_next <= counter;
-        coeff_read_out_next <= coeff_read_out;
+        mem_read_out_next <= mem_read_out;
         state_next <= state;
         case(state)
             IDLE: begin
@@ -129,7 +157,12 @@ module dsp_top(
                 end
             end
             COMPUTE: begin
-                if (counter < LATENCY) begin
+                if (new_symbol) begin
+                    counter_next <= 1'b1; 
+                    state_next <= COMPUTE;
+                    data_out_valid_next <= 0;
+                end
+                else if (counter < LATENCY) begin
                     data_out_valid_next <= 0;
                     state_next <= COMPUTE; 
                     counter_next <= counter + 1'b1;
@@ -146,17 +179,23 @@ module dsp_top(
             end
         endcase
 
-        // Set read output if address is valid and coeff_read flag is true
-        if (coeff_addr > 10'd127 & coeff_addr < 10'd199 & coeff_read)
-            coeff_read_out_next <= coeff_read_out_i;
-        else if (coeff_addr > 10'd255 & coeff_addr < 10'd327 & coeff_read)
-            coeff_read_out_next <= coeff_read_out_q;
+        // Set read output if address is valid and read flag is true
+        if (mem_addr > 10'd127 & mem_addr < 10'd199 & read)
+            mem_read_out_next <= coeff_read_out_i;
+        else if (mem_addr > 10'd255 & mem_addr < 10'd327 & read)
+            mem_read_out_next <= coeff_read_out_q;
+        
+        else if (mem_addr > 10'd511 & mem_addr < 10'd640 & read)
+            mem_read_out_next <= sample_read_out_i;
+        else if (mem_addr > 10'd767 & mem_addr < 10'd896 & read)
+            mem_read_out_next <= sample_read_out_q;
         else 
-            coeff_read_out_next <= 8'd0;
+            mem_read_out_next <= 8'd0;
+
 	end
 
     // Mulyiply outputs by valid flag to flush to zero if invalid
-    // Round-towards-zero rounding 
+    // Round-towards-zero rounding. This should probably be done more cleanly
     assign I_out = (filter_out_i < 0 && filter_out_i[1:0] != 2'b00) ? ((filter_out_i >> 2) * data_out_valid + 1) : ((filter_out_i >> 2) * data_out_valid);
     assign Q_out = (filter_out_q < 0 && filter_out_q[1:0] != 2'b00) ? ((filter_out_q >> 2) * data_out_valid + 1) : ((filter_out_q >> 2) * data_out_valid);
 
---
+++ project8/source/sample_storage.v
@@ -0,0 +1,99 @@
+`timescale 1ns /1ps
+///////////////////////////////
+// ECE 6214
+// Prof Matthew LaRue
+// Memory for filter outputs
+///////////////////////////////
+
+module sample_storage(
+	input wire  clk,
+    input wire  rst_n,        // ?
+	input wire signed [11:0]  sample_in,
+	input wire [9:0] 	    sample_addr,
+	// input wire 		  sample_write,
+	input wire 		  sample_read,
+    input wire         counter_begin,   // Signal to start writing to sample storage
+	output wire signed [7:0] sample_read_out
+);
+
+    // Internal signals
+    reg signed [7:0] sample_internal[0:127];
+	reg signed [7:0] sample_read_out_q;
+    
+    // Simple FSM Variables
+    parameter IDLE = 2'b00;
+    parameter WRITE = 2'b01;
+    parameter DONE = 2'b10;
+    reg [1:0] state;
+    reg [1:0] state_next; 
+    reg [5:0] counter;
+    reg [5:0] counter_next;
+
+    integer i;
+
+    // Sequential Logic
+	always @(posedge clk) begin
+        if (!rst_n) begin
+            state <= IDLE;
+            counter <= 6'd0;
+            // Flush internal memory
+            for (i=0; i<128; i++) begin
+                sample_internal[i] <= 8'h00;
+            end
+        end
+
+        else begin
+            counter <= counter_next;
+            state <= state_next;
+
+            // Handle filter output writes (only filter can write). Only write first 64 samples
+            if (state == WRITE) begin
+                sample_internal[counter * 2] <= sample_in[11:4];            // 8 MSBs 
+                sample_internal[counter * 2 + 1] <= {8'h0, sample_in[3:0]}  // 4 LSBs in addr + 1
+            end
+
+            // Handle coefficient read requests. We can only read 8 bits at a time, so only half of a filter output
+            if (sample_read == 1'b1) begin
+                sample_read_out_q <= sample_internal[sample_addr];
+            end
+            else if (sample_read == 1'b0) begin
+                sample_read_out_q <= 1'b0;
+            end
+        end
+    end
+
+    // Combinational Logic FSM
+    always @(*) begin
+        counter_next <= counter;
+        state_next <= state;
+        case(state)
+            IDLE: begin
+                if (counter_begin) begin
+                    state_next <= WRITE;
+                    counter_next <= 1'b0;
+                end
+                else begin
+                    state_next <= IDLE;
+                    counter_next <= 6'd0;
+                end
+            end
+            WRITE: begin
+                if (counter < 64) begin
+                    state_next <= WRITE;
+                    counter_next <= counter + 1'b1;
+                end
+                else begin      // Sample storage is full
+                    state_next <= DONE; 
+                    counter_next <= 6'd0;
+                end
+            end
+            DONE: begin         // Sample storage is full. Do nothing
+                state_next <= DONE; 
+                counter_next <= 6'd0;
+            end
+        endcase
+    end
+
+	assign sample_read_out = sample_read_out_q;
+endmodule
+   
